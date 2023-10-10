520ae68cb0ca6a41d8eaeae43044bfbd9637b46b
bhatiaarpit <118626392+bhatiaarpit@users.noreply.github.com>
2023-10-10 14:28:26 +0000

    Conversion of datatypes

---
+++ basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+// "33abc" => NaN 
+//true => 1; false => 0
+
+let isLoggedIn = "Arpit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+ 
+
+// 1=> true; 0=> false
+// "" => false
+// "arpit" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
