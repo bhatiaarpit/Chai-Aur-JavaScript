ca89ed27d67ae225109348070b5b39de799087d8
bhatiaarpit <118626392+bhatiaarpit@users.noreply.github.com>
2023-10-12 18:32:19 +0000

    Comparison & Datatypes

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+  // Primitive
+
+  // 7 Types : String, Number, boolean, null, undefined, Symbol, Bigint
+
+  const score = 100
+  const scorevalue = 100.3
+
+  const isLoggedIn = false
+  const outsideTemp = null
+
+  let userEmail;
+
+  const id = Symbol('123')
+  const anotherId = Symbol('123')
+
+  console.log(id === anotherId);
+
+  //Reference(Non-primitive)
+
+  //Array, Objects, Functions
+
+  const heros = ["shaktimaan" ,"naagraj" , "doga"];
+
+  let myObj ={
+    name: "hitesh",
+    age: 22,
+  }
+
+  const myFunction = function () {
+    console.log("Hello world");
+  }
+
+  console.log(typeof heros);
