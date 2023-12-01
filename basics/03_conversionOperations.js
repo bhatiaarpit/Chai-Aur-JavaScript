17aa428bcb06a0a3629f1d53226e0e078497afc6
Arpit Bhatia <118626392+bhatiaarpit@users.noreply.github.com>
2023-12-01 06:56:18 +0000

    operations

--- basics/03_conversionOperations.js
+++ basics/03_conversionOperations.js
@@ -13,7 +13,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "Arpit"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
  
 
 // 1=> true; 0=> false
@@ -23,5 +23,29 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber); // converted in string
+// console.log(stringNumber);
+// console.log(typeof stringNumber); // converted in string
+
+
+//*********************** Operations *********************//
+ 
+let value = 3
+let negvalue = -value
+// console.log(negValue)
+
+let str1 = "Hello"
+let str2 = " Arpit"
+// let str3 = console.log(str1 + str2);
+
+// console.log("1" + 2);     //12
+// console.log(1 + "2"); //12
+// console.log("1" + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log(+true); //1
+// console.log(+""); //0
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
