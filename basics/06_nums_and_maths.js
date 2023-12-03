46543e441627c2f9fda18b07799af4f915b983e2
Arpit Bhatia <118626392+bhatiaarpit@users.noreply.github.com>
2023-12-03 07:59:33 +0000

    number and maths

---
+++ basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+const score = 500
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const otherNumber = 23.8977
+// console.log(otherNumber.toPrecision((3)));
+
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++ Maths ++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.2));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.1));
+// console.log(Math.min(4,5,4,2,1));
+// console.log(Math.max(4,5,4,2,1));
+
+
+console.log(Math.round(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+ 
