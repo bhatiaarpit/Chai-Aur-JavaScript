b83f26e0e6a89e1b93719bf9ce159a5dc7960b63
Arpit Bhatia <118626392+bhatiaarpit@users.noreply.github.com>
2023-12-04 06:21:55 +0000

    slice splice

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+//array
+
+const myArr = [0,1,2,4,5]
+const myHeros = ["shaktimaan", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+//Array Methods
+
+// myArr.push(6)
+// myArr.pop()
+
+
+// myArr.unshift(0)
+// myArr.shift(0)
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+
+// const newArr = myArr.join()
+
+// console.log(newArr); //typeof = String
+// console.log(myArr); 
+
+//slice, splice
+
+console.log("A" , myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+
+
+
