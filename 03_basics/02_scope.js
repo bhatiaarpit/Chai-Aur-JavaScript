5d14c6d424f12f8b13cf93ff4f80c8f0bc09844a
Arpit Bhatia <118626392+bhatiaarpit@users.noreply.github.com>
2023-12-14 06:26:50 +0000

    mini hoisting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -3,10 +3,48 @@ let a = 300;
 if (true){
     let a = 10
     const b = 20
-    console.log("inner: ", a);
+    // console.log("inner: ", a);
 }
 
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+
+function one(){
+    const username = "arpit"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    // two()
+}
+// one()
+
+if (true) {
+    const username ="hitesh"
+    if (username === "arpt") {
+        const website = "youtube"
+        // console.log(username + website);        
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+// +++++++++++++++++++++++++++++++++++++
+// addone(5) //here you can access addone before initialization
+function addone(num){
+    return num + 1
+} 
+
+addone(5)
+
+// addTwo(5) //we can not access addtwo before initialization
+const addTwo = function (num) {
+    return num + 2
+}
+// addTwo(5)
