87791dde01da54f05da0ec12284794c4f5d551e0
Arpit Bhatia <118626392+bhatiaarpit@users.noreply.github.com>
2023-12-15 16:51:32 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");        
+        break;
+    case 2:
+        console.log("Feb");        
+        break;
+    case 3:
+        console.log("March");        
+        break;
+    case 4:
+        console.log("april");        
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+} 
