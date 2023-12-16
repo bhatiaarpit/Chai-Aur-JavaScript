d1e3cfe4c17c3d2dc76b0e68c25f3c750339ca08
Arpit Bhatia <118626392+bhatiaarpit@users.noreply.github.com>
2023-12-16 07:58:26 +0000

    break and continue

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -31,3 +31,21 @@ for (let i = 0; i <= 10; i++) {
     
 // }
 
+//break and continue
+
+for (let index = 1; index < 20; index++) {
+    if (index == 5) {
+        console.log(`detected 5`);
+        break
+    }
+    console.log(`value of i is ${index}`);
+    
+}
+for (let index = 1; index < 20; index++) {
+    if (index == 5) {
+        console.log(`detected 5`);
+        continue
+    }
+    console.log(`value of i is ${index}`);
+    
+}
