449ab1fc1fc2f2c35b849b3b14d30efa9b532f73
Arpit Bhatia <118626392+bhatiaarpit@users.noreply.github.com>
2023-12-16 07:57:58 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,33 @@
+// For Loop
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log("5 is the best Number");
+    }
+    // console.log(element);
+}
+
+// console.log(element);
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 0; j < 10; j++) {
+//         // console.log(`Inner loop value ${j} and inner loop ${i}`);
+//         console.log(i + '*' + j + '=' + i*j);
+//     }
+  
+// }
+
+
+
+
+// let myArray = ["flash" , "batman", "superman"]
+// // console.log(myArray.length);
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     // console.log(element);
+    
+// }
+
