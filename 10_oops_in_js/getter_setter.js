af60d7479e5310433dddafb4f14af2236d0fe71e
Arpit Bhatia <118626392+bhatiaarpit@users.noreply.github.com>
2024-02-12 19:40:49 +0000

    End of js..series

---
+++ 10_oops_in_js/getter_setter.js
@@ -0,0 +1,24 @@
+class User{
+    constructor(email, password){
+        this.email = email;
+        this.password = password;
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return this._password.toUpperCase()
+    }
+    set password(value){
+        this._password = value
+    }
+}
+
+const hitesh = new User("h@hitesh.ai", "abccc")
+console.log(hitesh.password);
